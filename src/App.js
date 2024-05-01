b0cfb355c305e790d0dd0e5cc8199b3e63c3ab24
Anthony-Wen <anthonywen04@gamil.com>
2024-04-30 18:40:44 -0700

    set up skeleton 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Page from "./Page";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Test from "./Test";
+import About_us from "./About_us";
 import Dashboard from "./Dashboard";
+import Food_Request from "./Food_Request";
+import Forms from "./Forms";
+import Login from "./Login";
+import Register from "./Register";
+import Map from "./Maps";
+import Table from "./Table";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
 
 const App = () => {
   return (
@@ -10,8 +19,17 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/" element={<Page />} />
-          <Route path="/test" element={<Test />} />
           <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/About_us" element={<About_us />} />
+          <Route path="/Food_Request" element={<Food_Request />} />
+          <Route path="/forms" element={<Forms />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/Table" element={<Table />} />
+          <Route path="/Navbar" element={<Navbar />} />
+          <Route path="/Sidebar" element={<Sidebar />} />
+          <Route path="/Footer" element={<Footer />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Food_Request.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Food_Request = () => {
+  return <div>Food_Request</div>;
+};
+
+export default Food_Request;
