073ec089fe8fb30014f0176aec70982d37956b5c
keatonstaggs <114716233+keatonstaggs@users.noreply.github.com>
2024-05-09 17:04:05 -0700

    map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Page from "./Page";
+// import Page from "./Page";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About_us from "./About_us";
 import Dashboard from "./Dashboard";
@@ -7,29 +7,29 @@ import Food_Request from "./Food_Request";
 import Forms from "./Forms";
 import Login from "./Login";
 import Register from "./Register";
-import Map from "./Maps";
+import MapComponent from "./Maps";
 import Table from "./Table";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 
 const App = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Page />} />
+          {/* <Route path="/" element={<Page />} /> */}
           <Route path="/Dashboard" element={<Dashboard />} />
           <Route path="/About_us" element={<About_us />} />
           <Route path="/Food_Request" element={<Food_Request />} />
           <Route path="/forms" element={<Forms />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
-          <Route path="/map" element={<Map />} />
+          <Route path="/map" element={<MapComponent />} />
           <Route path="/Table" element={<Table />} />
           <Route path="/Navbar" element={<Navbar />} />
           <Route path="/Sidebar" element={<Sidebar />} />
-          <Route path="/Footer" element={<Footer />} />
+          {/* <Route path="/Footer" element={<Footer />} /> */}
         </Routes>
       </div>
     </Router>
--- src/Maps.js
+++ src/Maps.js
@@ -1,7 +1,154 @@
 import React from "react";
+import { useEffect } from "react";
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import image from './images/uw-fb.jpg';
+import image2 from './images/frn-fb.webp';
+import image3 from './images/ud-fb.webp';
 
-const map = () => {
-  return <div>map</div>;
+
+const MapComponent = () => {
+  useEffect(() => {
+    mapboxgl.accessToken = "pk.eyJ1IjoiZmdvbnphMiIsImEiOiJjbG9vdWFjcHIwMG5kMmpvMXN5aWkwZXc4In0.GWMxQgk8OVnVI8DwIGcw_w";
+
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-122.31509338850782, 47.65922714668673],
+      zoom: 13.5,
+    });
+
+    new mapboxgl.Marker()
+      .setLngLat([-122.3176455646381, 47.6661020144631])
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(`<div class=map-popup-container> \
+            <img class="food-bank-image" src='${image3}' alt='University District Food Bank' /> \
+            <h2>University District Food Bank</h2> \
+            <p>5017 Roosevelt Way NE, Seattle, WA 98105</p> \
+            <p class=map-request>Requests</p> \
+            <hr /> \
+            <p class=map-food-type>Tinned Fruits</p> \
+          <p>Type:</p> \
+          <ul> \
+            <li>Apples</li> \
+            <li>Oranges</li> \
+            <li>Peaches</li> \
+            <li>Bananas</li> \
+            </ul> \
+            <hr /> \
+            <p class=map-food-type>Canned Proteins</p> \
+            <p>Type:</p> \
+            <ul> \
+              <li>Salmon</li> \
+              <li>Blackbeans</li> \
+              <li>Tuna</li> \
+              <li>Chickpeas</li> \
+              </ul> \
+              <hr /> \
+            <p class=map-food-type>Canned Vegetables</p> \
+            <p>Type:</p> \
+            <ul> \
+              <li>Spinach</li> \
+              <li>Olive</li> \
+              <li>Corn</li> \
+              <li>Tomato</li> \
+              </ul> \
+            </div>`
+          )
+      )
+      .addTo(map);
+
+    new mapboxgl.Marker()
+      .setLngLat([-122.31413627931678, 47.656910115665006])
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(`<div class=map-popup-container> \
+          <img class="food-bank-image" src='${image}' alt='UW Food Pantry Food Bank' /> \
+          <h2>UW Food Pantry</h2> \
+          <p>3900 E Stevens Way NE, Seattle, WA 98195</p> \
+          <p class=map-request>Requests</p> \
+          <hr /> \
+          <p class=map-food-type>Tinned Fruits</p> \
+          <p>Type:</p> \
+          <ul> \
+            <li>Apples</li> \
+            <li>Oranges</li> \
+            <li>Peaches</li> \
+            <li>Bananas</li> \
+            </ul> \
+            <hr /> \
+            <p class=map-food-type>Canned Proteins</p> \
+            <p>Type:</p> \
+            <ul> \
+              <li>Salmon</li> \
+              <li>Blackbeans</li> \
+              <li>Tuna</li> \
+              <li>Chickpeas</li> \
+              </ul> \
+              <hr /> \
+            <p class=map-food-type>Canned Vegetables</p> \
+            <p>Type:</p> \
+            <ul> \
+              <li>Spinach</li> \
+              <li>Olive</li> \
+              <li>Corn</li> \
+              <li>Tomato</li> \
+              </ul> \
+            </div>`
+        )
+      )
+      .addTo(map);
+
+    new mapboxgl.Marker()
+      .setLngLat([-122.3120508748807, 47.66491915480083])
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(`<div class=map-popup-container> \
+          <img class="food-bank-image" src='${image2}' alt='Food Resource Network' /> \
+          <h2>Food Resource Network</h2> \
+          <p>1413 NE 50th St, Seattle, WA 98105</p> \
+          <p class=map-request>Requests</p> \
+          <hr /> \
+          <p class=map-food-type>Tinned Fruits</p> \
+          <p>Type:</p> \
+          <ul> \
+            <li>Apples</li> \
+            <li>Oranges</li> \
+            <li>Peaches</li> \
+            <li>Bananas</li> \
+            </ul> \
+            <hr /> \
+            <p class=map-food-type>Canned Proteins</p> \
+            <p>Type:</p> \
+            <ul> \
+              <li>Salmon</li> \
+              <li>Blackbeans</li> \
+              <li>Tuna</li> \
+              <li>Chickpeas</li> \
+              </ul> \
+              <hr /> \
+            <p class=map-food-type>Canned Vegetables</p> \
+            <p>Type:</p> \
+            <ul> \
+              <li>Spinach</li> \
+              <li>Olive</li> \
+              <li>Corn</li> \
+              <li>Tomato</li> \
+              </ul> \
+            </div>`
+        )
+      )
+      .addTo(map);
+
+      return () => map.remove();
+  }
+  , []);
+  return <div id="map" style={{width: '100%', height: '100vh'}} />
+  
 };
 
-export default map;
+export default MapComponent;
+
+
+
