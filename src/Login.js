786bb1ed570e0a42432304e14de250d423d216ca
Ceciliai <seaoneseeyou@gmail.com>
2024-05-13 19:50:54 -0700

    one page look's good now

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "./index.css";
+import logo from "./images/KACK_logo.png";
 
-const Login = () => {
-  return <div>Login</div>;
-};
+function Login() {
+  return (
+    <div className="kackLoginBody">
+      <div className="kackLoginContainer">
+        <div className="kackLogoContainer">
+          <img src={logo} alt="KACK FOOD BANK Logo" className="kackLoginLogo" />
+        </div>
+
+        <div className="login-register">
+          <h2 className="kackLoginH2">
+            Choose your identity to login/Register:
+          </h2>
+          <a href="login_foodbank.html" className="kackLoginOptionButton">
+            FOOD BANK
+          </a>
+          <a href="login_donors.html" className="kackLoginOptionButton">
+            DONOR
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
